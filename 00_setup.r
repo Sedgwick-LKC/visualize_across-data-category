e2d320e84adaf41fe3c510cd56a2677042bffb54
njlyon0 <nicholasjlyon@gmail.com>
2025-11-10 11:56:32 -0500

    feat (setup): first pass at dedicated setup script

---
+++ 00_setup.r
@@ -0,0 +1,20 @@
+## ------------------------------------------------ ##
+# Visualize - Setup
+## ------------------------------------------------ ##
+# Purpose:
+## Do setup steps that are necessary for at least one of the visualization scripts in this repo
+
+# Clear environment
+rm(list = ls()); gc()
+
+## ----------------------------- ##
+# Make Folders ----
+## ----------------------------- ##
+
+# Create necessary folders
+dir.create(path = file.path("data", "raw"), showWarnings = F, recursive = T)
+dir.create(path = file.path("data", "tidy"), showWarnings = F)
+dir.create(path = file.path("data", "precip"), showWarnings = F)
+dir.create(path = file.path("graphs"), showWarnings = F)
+
+# End ----
